6bc1b031d44fa629d623fd4fdc613ee1e4e8b7f8
ionut.schiopu <ionut.schiopu@nagarro.com>
2024-07-02 13:23:02 +0300

    remove comments

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,55 +1,46 @@
 /**
- * The base URL for the API.
  * @constant
  */
  export const POKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20';
 
   /**
-  * The maximum number of retries for API requests.
   * @constant
   */
 export const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 /**
-  * The maximum number of retries for API requests.
   * @constant
   */
 export const POKEMON_SPECIES_URL = 'https://pokeapi.co/api/v2/pokemon-species';
 
 /**
-  * The maximum number of retries for API requests.
   * @constant
   */
 export const POKEMON_EVOLUTIONS_URL = 'https://pokeapi.co/api/v2/evolution-chain';
 
  /**
-  * The maximum number of retries for API requests.
   * @constant
   */
 export const POKEMON_TYPE_URL = 'https://pokeapi.co/api/v2/type';
 
 
 /**
-  * The maximum number of retries for API requests.
   * @constant
 */
 export const POKEMON_IMG_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 
 /**
-  * The maximum number of retries for API requests.
   * @constant
   */
 export const POKEMON_DAMAGE_FIRST_CLASS_URL = 'https://pokeapi.co/api/v2/move-damage-class/1/';
 
 /**
-  * The maximum number of retries for API requests.
   * @constant
   */
 export const POKEMON_DAMAGE_SECOND_CLASS_URL = 'https://pokeapi.co/api/v2/move-damage-class/2/';
 
  /**
-  * The maximum number of retries for API requests.
   * @constant
   */
 export const POKEMON_DAMAGE_THIRD_CLASS_URL = 'https://pokeapi.co/api/v2/move-damage-class/3/';
